ae0903657b7241d53fe23133b27af22bc7ed0a14
luxyva <luxyva@outlook.com>
2020-05-19 21:27:11 +0800

    add service base (service provider)

---
+++ base/src/test/java/io/luxyva/switches/base/BaseApplicationTests.java
@@ -0,0 +1,12 @@
+package io.luxyva.switches.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
