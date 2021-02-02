0da686c7f5d6936a1e5247e99c5c790b61664185
luxyva <luxyva@outlook.com>
2021-02-02 22:28:11 +0800

    Add Mybatis

--- base/src/main/java/io/luxyva/switches/base/BaseApplication.java
+++ base/src/main/java/io/luxyva/switches/base/BaseApplication.java
@@ -1,11 +1,13 @@
 package io.luxyva.switches.base;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@MapperScan("com.example.demo.mapper")
 public class BaseApplication {
 
     public static void main(String[] args) {
